72f968537cfb8f0406cfac8ab66839555efa3288
Pandeymanish1999 <manish.22gcemca037@galgotiacollege.edu>
2023-09-27 12:34:49 +0530

    Second commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    int sum  = 0 ;
+    sum = a+b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<sum;
+    return 0;
+}
